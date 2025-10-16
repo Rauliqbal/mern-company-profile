a0a896f137fdd0a19840ea6f211b44c4da20c53d
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2025-10-16 20:45:20 +0700

    chore: setup tailwind

--- frontend/src/components/sections/Hero.tsx
+++ frontend/src/components/sections/Hero.tsx
@@ -1,6 +1,10 @@
+import bgHero from '../../assets/images/hero.png'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div className="h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: `url(${bgHero})` }}>
+      <h1>Grow your business faster</h1>
+    </div>
   )
 }
